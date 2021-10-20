f675b2649d2818daf48079c5988790c1b7f9ff4c
Bharat Dev Burman <bharat.singh7924@gmail.com>
2021-10-20 11:12:47 +0530

    don't export room database schema
    Signed-off-by: Bharat Dev Burman <bharat.singh7924@gmail.com>

--- app/src/main/java/com/example/task/incoming/data/IncomingCallDatabase.kt
+++ app/src/main/java/com/example/task/incoming/data/IncomingCallDatabase.kt
@@ -3,7 +3,7 @@ package com.example.task.incoming.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [IncomingCall::class], version = 1)
+@Database(entities = [IncomingCall::class], version = 1, exportSchema = false)
 abstract class IncomingCallDatabase : RoomDatabase() {
     abstract fun incomingCallDao(): IncomingCallDao
 }
