0ef6508a4e79e8e81b96c2a49b2ab65206db7386
Bharat Dev Burman <bharat.singh7924@gmail.com>
2024-05-27 01:21:55 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,9 @@ buildscript {
         mavenCentral()
         
     }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.0.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
+    plugins {
+        id("com.android.application") version "8.4.0" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
 
 allprojects {
