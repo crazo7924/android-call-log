999025f354743cc1bcb88309e1cd5f1e017f5489
Bharat Dev Burman <bharat.singh7924@gmail.com>
2024-05-27 10:15:43 +0530

    cleanup build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-plugins {
-    id("com.android.application") version "8.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-}
-
 allprojects {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
