4233ecc7f290e334675b6e8d61e2e6165af386d3
Bharat Dev Burman <bharat.singh7924@gmail.com>
2024-05-27 22:22:47 +0530

    Update build.gradle.kts (#7)
    * Update build.gradle.kts
    
    * add missing closing bracket
    
    * move plugins block to root
    
    * cleanup build.gradle.kts
    
    * Update settings.gradle.kts
    
    * gradlew: bump to 8.7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,15 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+}
+
 rootProject.name = "Android call logger"
 include(":app")
